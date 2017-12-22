1097b4057ac1255c32562e2d5f189000b75c1ab2
martinproxy0 <marcinsornek@gmail.com>
2017-12-23 00:02:07 +0100

    Add code for exercise 12.4

---
+++ main.js
@@ -0,0 +1,23 @@
+var buttonTranslet = $('#translate-joke');
+var parTrans = $('#transleted-joke');
+
+var url = 'http://api.icndb.com/jokes/random';
+var button = $('#get-joke');
+var paragraph = $('#joke');
+
+
+$(function() {
+    button.on('click', function() {
+        getJoke();
+    });
+});
+
+function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function() {
+        var response = JSON.parse(xhr.response);
+        paragraph.html(response.value.joke);
+    });
+    xhr.send();
+}
